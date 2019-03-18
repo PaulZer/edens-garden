4805d38b9e2104bcd718fdf0015c282ca212fa55
PaulZer <bouchilloupaul@gmail.com>
2019-03-18 11:28:13 +0100

    minor form style

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -16,7 +17,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
+            ->add('firstName',)
             ->add('lastName')
             ->add('email')
             ->add('plainPassword', RepeatedType::class, [
@@ -40,6 +41,9 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('save', SubmitType::class, [
+                'label' => 'S\'inscrire',
+            ]);
         ;
     }
 
