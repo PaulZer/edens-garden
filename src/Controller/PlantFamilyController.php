483e3a168a8bfd3a436d2b5866b7516996f29e12
Elliot <elliot.edwards38@gmail.com>
2019-03-20 15:47:23 +0100

    New routes plant family and plant with list and by id

--- src/Controller/PlantFamilyController.php
+++ src/Controller/PlantFamilyController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Plant\PlantFamily;
+use App\Entity\Plant\Plant;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,8 +17,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class PlantFamilyController extends AbstractController
 {
-    
-
     public function viewAll(): Response
     {
         $plantFamilyRepository = $this->getDoctrine()
@@ -34,5 +33,40 @@ class PlantFamilyController extends AbstractController
         return $this->render('plant/plantFamilies.html.twig', [
             'families' => $plantFamilies,
         ]);
+    }
+
+    public function viewFamily(int $id): Response
+    {
+        $plantFamilyRepository = $this->getDoctrine()
+            ->getRepository(PlantFamily::class);
+
+        $plantRepository = $this->getDoctrine()
+            ->getRepository(Plant::class);
+            
+
+        if (!$plantFamilyRepository) {
+            throw $this->createNotFoundException(
+                'Please add plant families to continue'
+            );
+        }
+
+        if (!$plantRepository) {
+            throw $this->createNotFoundException(
+                'Please add a plant to this plant family to continue'
+            );
+        }
+
+        $plants = $plantRepository->findBy(
+            ['plantFamily' => $id]
+        );
+
+        
+        $plantFamilies = $plantFamilyRepository->find($id);
+
+        return $this->render('plant/plantFamily.html.twig', [
+            'family' => $plantFamilies,
+            'plants' => $plants
+        ]);
+
     }
 }
