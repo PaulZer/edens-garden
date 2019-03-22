629ce8762775e4f16c54d61177b297b1cfbfa5e5
PaulZer <bouchilloupaul@gmail.com>
2019-03-22 12:25:07 +0100

    [garden form] edit functionnality, display

--- src/Controller/GardenController.php
+++ src/Controller/GardenController.php
@@ -50,7 +50,7 @@ class GardenController extends AbstractController
         return $this->render('garden/garden.html.twig', ['garden' => $garden]);
     }
   
-    public function createGarden(Request $request):Response
+    /*public function createGarden(Request $request):Response
     {
         $garden = new Garden($this->getUser(), '', 0, 0, 0, 0);
         $form = $this->createForm(GardenType::class, $garden, ['action' => $this->generateUrl('garden_create')]);
@@ -69,6 +69,39 @@ class GardenController extends AbstractController
         return $this->render('garden/modals.html.twig', [
             'modalTitle' => 'Créer un jardin',
             'template' => 'form_garden',
+            'view' => $form->createView()
+        ]);
+    }*/
+
+    public function editGarden(Request $request, int $id = null):Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if($id > 0) $garden = $em->getRepository(Garden::class)->find($id);
+        else $garden = new Garden($this->getUser(), '', 0, 0, 0, 0);
+
+        $formAction = $request->attributes->get('_route') == 'garden_create' ? $this->generateUrl('garden_create'): $this->generateUrl('garden_edit', ['id' => $garden->getId()]);
+
+        $form = $this->createForm(GardenType::class, $garden, [
+            'action' => $formAction]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($garden);
+            $em->flush();
+
+            if($request->attributes->get('_route') == 'garden_edit'){
+                $word = 'modifié';
+            } else $word = 'créé';
+
+            $this->addFlash('success', "Votre jardin a été ".$word." avec succès ! Vous pouvez ajouter des plantes.");
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('modals.html.twig', [
+            'modalTitle' => $request->attributes->get('_route') == 'garden_create' ? 'Créer un jardin': $garden->getName().' - Édition',
+            'template' => 'garden/form_garden',
             'view' => $form->createView()
         ]);
     }
