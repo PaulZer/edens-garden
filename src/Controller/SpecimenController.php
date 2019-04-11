bd0d1b487276764b80e4b9174775a9d383455962
PaulZer <bouchilloupaul@gmail.com>
2019-04-11 16:43:58 +0200

    post-merge fix

--- src/Controller/SpecimenController.php
+++ src/Controller/SpecimenController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\SpecimenService;
+use App\Repository\SpecimenRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
