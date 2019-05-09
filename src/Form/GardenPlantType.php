01295e15cf7dc30701d89a2ef3fc2a0b5c708bd5
Elliot <elliot.edwards38@gmail.com>
2019-05-09 16:12:35 +0200

    Plant modal modifications

--- src/Form/GardenPlantType.php
+++ src/Form/GardenPlantType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -18,7 +19,10 @@ class GardenPlantType extends AbstractType
             ->add('name', TextType::class, [
                 'label' => 'Choisissez un nom :',
                 'attr' => ['maxlength' => 80]
-            ]);
+            ])
+            ->add('save', SubmitType::class, [
+            'label' => 'Valider',
+        ]);
 
     }
 }
